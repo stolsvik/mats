4731bf52a323f3b72cdbd4c99c9b4b9a4d3099bf
Endre Stølsvik <endre@stolsvik.com>
2016-11-05 12:04:23 +0100

    First hit at a Spring Boot AMQ with the webconsole hacked into place. WIP.

--- mats-amq-monitor/src/test/java/com/stolsvik/mats/amq/MatsAmqMonitorApplicationTests.java
+++ mats-amq-monitor/src/test/java/com/stolsvik/mats/amq/MatsAmqMonitorApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MatsAmqApplicationTests {
+public class MatsAmqMonitorApplicationTests {
 
     @Test
     public void contextLoads() {
