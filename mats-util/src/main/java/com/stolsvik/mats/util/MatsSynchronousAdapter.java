df90a22550006d2385231ee823aa6b154ebe0468
Endre Stølsvik <endre@stolsvik.com>
2018-08-19 02:06:10 +0200

    Resolves #7: Implemented MatsSynchronousAdapter.

---
+++ mats-util/src/main/java/com/stolsvik/mats/util/MatsSynchronousAdapter.java
@@ -0,0 +1,334 @@
+package com.stolsvik.mats.util;
+
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import com.stolsvik.mats.MatsEndpoint;
+import com.stolsvik.mats.MatsEndpoint.DetachedProcessContext;
+import com.stolsvik.mats.MatsFactory;
+import com.stolsvik.mats.MatsInitiator;
+import com.stolsvik.mats.MatsInitiator.InitiateLambda;
+import com.stolsvik.mats.MatsInitiator.MatsInitiate;
+
+/**
+ * This is a tool that should have a very limited set of use cases: Processes that by nature are synchronous, but needs
+ * to call into the "MATS network" of services. This enables a thread to issue a request to a Mats endpoint, and then
+ * synchronously wait for the service to reply. You should not really use it, ever! However, it provides a way to invoke
+ * a Mats endpoint, and getting a {@link CompletableFuture} back - which is very convenient when e.g. implementing a
+ * traditional blocking, synchronous HTTP request - or a public-static-void-main "script", or an integration test.
+ * <p>
+ * The class implements {@link AutoCloseable}, but this is meant for e.g. Spring environments which closes beans, and
+ * does not imply that this class should be closed after each usage, i.e. do not use try-with-resources constructs. The
+ * close method should be invoked when the application shuts down, not after e.g. each message.
+ * <p>
+ * This is built upon the MATS API, not tying into any of the implementation details.
+ */
+public class MatsSynchronousAdapter<R> implements AutoCloseable {
+    private static final Logger log = LoggerFactory.getLogger(MatsSynchronousAdapter.class);
+
+    public static <R> MatsSynchronousAdapter<R> create(MatsFactory matsFactory, MatsInitiator matsInitiator,
+            String returnEndpointPrefix, Class<R> replyClass) {
+        return new MatsSynchronousAdapter<>(matsFactory, matsInitiator, returnEndpointPrefix, replyClass);
+    }
+
+    public static class Reply<R> {
+        private final DetachedProcessContext _processContext;
+        private final R _reply;
+        private final double _latency;
+
+        Reply(DetachedProcessContext processContext, R reply, double latency) {
+            _reply = reply;
+            _processContext = processContext;
+            _latency = latency;
+        }
+
+        public R getReply() {
+            return _reply;
+        }
+
+        public DetachedProcessContext getProcessContext() {
+            return _processContext;
+        }
+
+        public double getLatencyMillis() {
+            return _latency;
+        }
+    }
+
+    private final MatsInitiator _matsInitiator;
+    private final String _completingSubscriptionEndpointId;
+    private final MatsEndpoint<String, Void> _completingSubscriptionTerminator;
+
+    private MatsSynchronousAdapter(MatsFactory matsFactory, MatsInitiator matsInitiator, String returnEndpointIdPrefix,
+            Class<R> replyClass) {
+        _matsInitiator = matsInitiator;
+
+        _completingSubscriptionEndpointId = returnEndpointIdPrefix + ".private.SyncAdapter."
+                + replyClass.getSimpleName() + "." + matsFactory.getFactoryConfig().getNodename();
+
+        // :: Create the Thread that will timeout futures
+        Thread timeoutThread = new Thread(this::timeouter,
+                "MATS:SynchronousAdapterTimeoutThread:" + returnEndpointIdPrefix);
+        timeoutThread.setDaemon(true);
+        timeoutThread.start();
+
+        // :: Create the SubscriptionTerminator that will complete futures.
+        _completingSubscriptionTerminator = matsFactory.subscriptionTerminator(_completingSubscriptionEndpointId,
+                replyClass, String.class,
+                (context, reply, correlationId) -> {
+                    // Find and remove the Future's promise, using the state String which acts as Correlation Id.
+                    WaitingPromise<R> promise;
+                    synchronized (_outstandingPromises) {
+                        promise = _outstandingPromises.remove(correlationId);
+                        // Notify the Timeout Thread so that it can re-calculate when to wake up.
+                        _outstandingPromises.notifyAll();
+                    }
+                    // ?: Did we find the promise?
+                    if (promise == null) {
+                        // -> No, promise not present, so log a warning.
+                        log.warn("WAITING PROMISE GONE: Got a reply from [" + context.getFromStageId()
+                                + "], but the correlationId [" + correlationId + "] didn't map to a waiting promise."
+                                + " It had probably already been completed exceptionally with "
+                                + MatsFutureTimeoutException.class.getSimpleName() + " (thus, the future.get()-call"
+                                + " got an ExecutionException)");
+                        return;
+                    }
+                    // E-> Yes, promise is still present, so complete it.
+                    double latency = (System.nanoTime() - promise._startNanos) / 1_000_000d;
+                    promise._future.complete(new Reply<>(context, reply, latency));
+
+                    if (log.isDebugEnabled())
+                        log.debug("COMPLETED FUTURE: We found the CorrelationId [" + correlationId
+                                + "] for reply from [" + context.getFromStageId()
+                                + "], and completed the Future with TraceId:[" + promise._traceId
+                                + "] and FromId:[" + promise._fromId + "].");
+                });
+    }
+
+    private boolean _runTimeoutThread = true;
+
+    /**
+     * Stops the Timeout Thread and cancels all outstanding Futures.
+     */
+    public void close() {
+        synchronized (_outstandingPromises) {
+            // Set the TimeoutThread to exit
+            _runTimeoutThread = false;
+            // Cancel all Futures
+            _outstandingPromises.values().forEach(waitingPromise -> waitingPromise._future.cancel(true));
+            // Clear the Map (all are cancelled!)
+            _outstandingPromises.clear();
+            // Ping the Timeout Thread so that it notices that it should exit.
+            _outstandingPromises.notifyAll();
+        }
+    }
+
+    private final HashMap<String, WaitingPromise<R>> _outstandingPromises = new HashMap<>();
+
+    private void timeouter() {
+        log.info("Timeout Thread for MatsSynchronousAdapter [" + _completingSubscriptionEndpointId + "] started.");
+        synchronized (_outstandingPromises) {
+            while (_runTimeoutThread) {
+                // :: Find the nearest Future to time out.
+                long nextTimeout = _outstandingPromises.values().stream()
+                        .mapToLong(value -> value._timeoutAt)
+                        .min()
+                        // If none, then 15 minute delay, to just say "hi, I am still here" if there are no futures.
+                        .orElse(-1);
+
+                // ?: Was there any outstanding Futures?
+                if (nextTimeout == -1) {
+                    // -> No, no outstanding Futures. Log this just to get some liveliness indication in log.
+                    log.info("Currently no outstanding Futures.");
+                    // Set next timeout to 15 minutes from now, so that we get periodic "I am alive!" log lines.
+                    nextTimeout = System.currentTimeMillis() + (15 * 60 * 1000);
+                }
+
+                // :: Wait till the next Future is overdue, if none, then the check above ensure 15 mins
+                long waitTime = Long.max(0, nextTimeout - System.currentTimeMillis());
+                // ?: Are we already overdue for any Future?
+                if (waitTime > 0) {
+                    // -> No, we should wait
+                    try {
+                        // Wait a tad longer to account for possible a-bit-early wakeup.
+                        _outstandingPromises.wait(waitTime + 25);
+                    }
+                    catch (InterruptedException e) {
+                        log.warn("Interrupted - which is weird, since no interrupts are used here.");
+                    }
+                }
+
+                // :: For all Futures that are overdue: resolveExceptionally(MatsFutureTimeoutException), and remove.
+                _outstandingPromises.values().removeIf(promise -> {
+                    // ?: Are we past the timeoutAt time?
+                    if (System.currentTimeMillis() > promise._timeoutAt) {
+                        try {
+                            MDC.put("traceId", promise._traceId);
+                            log.warn("Timed out Future [" + promise._future + "] with TraceId:[" + promise._traceId
+                                    + "] and FromId: [" + promise._fromId + "].");
+                        }
+                        finally {
+                            MDC.remove("traceId");
+                        }
+                        // -> Yes, so time it out by completeExceptionally, and return true to remove it.
+                        promise._future.completeExceptionally(new MatsFutureTimeoutException(
+                                "Future with TraceId:[" + promise._traceId + "] and"
+                                        + " FromId:[" + promise._fromId + "]:"
+                                        + " Waited more than the specified " + promise._timeoutMillis
+                                        + " ms for this Future to complete."));
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+        log.info("Timeout Thread for MatsSynchronousAdapter [" + _completingSubscriptionEndpointId + "] shut down.");
+    }
+
+    public static class MatsFutureTimeoutException extends Exception {
+        MatsFutureTimeoutException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Base method, with all the features exposed.
+     *
+     * @param timeoutMillis
+     *            how long time, in milliseconds, before the CompletableFuture completes exceptionally with a
+     *            {@link MatsFutureTimeoutException}.
+     * @param traceId
+     *            The tradeId for the request and resulting call flow - read guidelines for TraceIds in MATS API.
+     * @param from
+     *            who this is "from" - i.e. a virtual/synthetic endpointId that describes who is sending the request -
+     *            for debugging and call tracing.
+     * @param to
+     *            the endpointId that should get the request
+     * @param requestDto
+     *            the request object that should be sent to the requested endpoint
+     * @param messageCustomizer
+     *            provides a way to customize the initiation, e.g. call {@link MatsInitiate#interactive()
+     *            MatsInitiate.interactive()}.
+     *
+     * @return a {@link CompletableFuture} which completes with a {@link Reply} instance, containing both the actual
+     *         reply, and a {@link DetachedProcessContext} from which to retrieve meta information, "sticky properties",
+     *         and any "side loaded" bytes or strings.
+     */
+    public CompletableFuture<Reply<R>> futureRequest(int timeoutMillis, String traceId, String from,
+            String to, Object requestDto, InitiateLambda messageCustomizer) {
+
+        // :: Should not send requests until we know that the completing endpoint has started (since it is a topic, any
+        // messages sent to it while not started will just be dumped on the floor by the MQ Broker).
+        _completingSubscriptionTerminator.waitForStarted();
+
+        // :: Make the CompletableFuture that we'll return.
+        CompletableFuture<Reply<R>> future = new CompletableFuture<>();
+
+        // Create a Correlation Id based on the TraceId, but we don't trust the user to make a properly unique traceId.
+        String correlationId = traceId + ":" + RandomString.randomCorrelationId();
+
+        // Add the corresponding "WaitingPromise" for this CompletableFuture, and notify the timeoutThread.
+        synchronized (_outstandingPromises) {
+            _outstandingPromises.put(correlationId, new WaitingPromise<>(future, from,
+                    correlationId, timeoutMillis, System.currentTimeMillis() + timeoutMillis));
+            _outstandingPromises.notifyAll();
+        }
+
+        // Create the request.
+        _matsInitiator.initiate(msg -> {
+            // Set up the message
+            msg.traceId(traceId)
+                    .to(to)
+                    .from(from)
+                    .replyToSubscription(_completingSubscriptionEndpointId, correlationId);
+
+            // Invoke the customizer.
+            messageCustomizer.initiate(msg);
+
+            // Pack it off
+            msg.request(requestDto);
+        });
+
+        return future;
+    }
+
+    /**
+     * Variant of {@link #futureRequest(int, String, String, String, Object, InitiateLambda) futureRequest(...)} that
+     * sets the {@link MatsInitiate#interactive() interactive} and {@link MatsInitiate#nonPersistent()} flags.
+     *
+     * @param timeoutMillis
+     *            how long time, in milliseconds, before the CompletableFuture completes exceptionally with a
+     *            {@link MatsFutureTimeoutException}.
+     * @param traceId
+     *            The tradeId for the request and resulting call flow - read guidelines for TraceIds in MATS API.
+     * @param from
+     *            who this is "from" - i.e. a virtual/synthetic endpointId that describes who is sending the request -
+     *            for debugging and call tracing.
+     * @param to
+     *            the endpointId that should get the request
+     * @param requestDto
+     *            the request object that should be sent to the requested endpoint
+     *
+     * @return a {@link CompletableFuture} which completes with a {@link Reply} instance, containing both the actual
+     *         reply, and a {@link DetachedProcessContext} from which to retrieve meta information, "sticky properties",
+     *         and any "side loaded" bytes or strings.
+     */
+    public CompletableFuture<Reply<R>> interactiveNonPersistentRequest(int timeoutMillis, String traceId, String from,
+            String to, Object requestDto) {
+        return futureRequest(timeoutMillis, traceId, from, to, requestDto, msg -> {
+            msg.interactive();
+            msg.nonPersistent();
+        });
+    }
+
+    /**
+     * Variant of {@link #interactiveNonPersistentRequest(int, String, String, String, Object)
+     * interactiveNonPersistentRequest(...)} that in addition to setting the the {@link MatsInitiate#interactive()
+     * interactive} and {@link MatsInitiate#nonPersistent()} flag, also specifies the timeout of 5 seconds - which is
+     * <i>way</i> more than what you should aim for wrt. the latencies of your services! (Less than 100 ms should be a
+     * goal).
+     *
+     * @param traceId
+     *            The tradeId for the request and resulting call flow - read guidelines for TraceIds in MATS API.
+     * @param from
+     *            who this is "from" - i.e. a virtual/synthetic endpointId that describes who is sending the request -
+     *            for debugging and call tracing.
+     * @param to
+     *            the endpointId that should get the request
+     * @param requestDto
+     *            the request object that should be sent to the requested endpoint
+     *
+     * @return a {@link CompletableFuture} which completes with a {@link Reply} instance, containing both the actual
+     *         reply, and a {@link DetachedProcessContext} from which to retrieve meta information, "sticky properties",
+     *         and any "side loaded" bytes or strings.
+     */
+    public CompletableFuture<Reply<R>> interactiveNonPersistentRequest(String traceId, String from, String to,
+            Object requestDto) {
+        return interactiveNonPersistentRequest(5000, traceId, from, to, requestDto);
+    }
+
+    /**
+     * This is the Promise of the Future, representing the "backside" of the returned futures.
+     */
+    private static class WaitingPromise<R> {
+        private final CompletableFuture<Reply<R>> _future;
+        private final String _fromId;
+        private final String _traceId;
+        private final long _startNanos = System.nanoTime();
+        private final int _timeoutMillis;
+        private final long _timeoutAt;
+
+        public WaitingPromise(CompletableFuture<Reply<R>> future, String fromId,
+                String traceId, int timeoutMillis, long timeoutAt) {
+            _future = future;
+            _fromId = fromId;
+            _traceId = traceId;
+            _timeoutMillis = timeoutMillis;
+            _timeoutAt = timeoutAt;
+        }
+    }
+}
---
+++ mats-util/src/test/java/com/stolsvik/mats/util/Test_MatsSynchronousAdapterTimeouts.java
@@ -0,0 +1,92 @@
+package com.stolsvik.mats.util;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.stolsvik.mats.exceptions.MatsRefuseMessageException;
+import com.stolsvik.mats.lib_test.DataTO;
+import com.stolsvik.mats.lib_test.MatsBasicTest;
+import com.stolsvik.mats.util.MatsSynchronousAdapter.Reply;
+
+/**
+ * Tests timeouts functionality of the {@link MatsSynchronousAdapter}.
+ */
+public class Test_MatsSynchronousAdapterTimeouts extends MatsBasicTest {
+
+    @Before
+    public void setupService() {
+        matsRule.getMatsFactory().single(SERVICE, DataTO.class, DataTO.class,
+                (context, msg) -> {
+                    log.info("Inside SERVICE which will wait 250 ms, context:\n" + context);
+                    try {
+                        Thread.sleep(250);
+                    }
+                    catch (InterruptedException e) {
+                        throw new MatsRefuseMessageException("Shouldn't get Interrupted here.");
+                    }
+                    return new DataTO(msg.number * 2, msg.string + ":FromService");
+                });
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void shallTimeoutInsideSynchronousAdapter() throws ExecutionException, InterruptedException,
+            TimeoutException {
+        // NOTE: Using try-with-resources in this test - not to be used in normal circumstances.
+        try (MatsSynchronousAdapter<DataTO> synchronousAdapter = MatsSynchronousAdapter.create(matsRule
+                .getMatsFactory(), matsRule.getMatsInitiator(), this.getClass().getSimpleName(), DataTO.class)) {
+
+            DataTO dto = new DataTO(42, "TheAnswer");
+
+            CompletableFuture<Reply<DataTO>> future = synchronousAdapter.futureRequest(75, randomId(),
+                    INITIATOR, SERVICE, dto, (msg) -> {});
+
+            // This timeout will be sent by the SynchronousAdapter, since the future was created with a too short
+            // timeout for the service to complete. The timeout-thread inside the SynchronousAdapter will thus
+            // complete this future exceptionally, i.e. ExecutionException.
+            thrown.expect(ExecutionException.class);
+
+            // This shall not go through, but throw
+            Reply<DataTO> result = future.get(1, TimeUnit.SECONDS);
+
+            Assert.fail("The future.get(1 sec) call should not have gone through, since the future was created with"
+                    + " only 75 milliseconds to wait for the completion of the service. The result was: " + result);
+        }
+    }
+
+    @Test
+    public void shallTimeoutInTheFutureGetInvocation() throws ExecutionException, InterruptedException,
+            TimeoutException {
+        // NOTE: Using try-with-resources in this test - not to be used in normal circumstances.
+        try (MatsSynchronousAdapter<DataTO> synchronousAdapter = MatsSynchronousAdapter.create(matsRule
+                .getMatsFactory(), matsRule.getMatsInitiator(), this.getClass().getSimpleName(), DataTO.class)) {
+
+            DataTO dto = new DataTO(42, "TheAnswer");
+
+            CompletableFuture<Reply<DataTO>> future = synchronousAdapter.futureRequest(2000, randomId(),
+                    INITIATOR, SERVICE, dto, (msg) -> {});
+
+            // This timeout will sent "synchronously" by the future.get() call itself, since it specified a too short
+            // wait for the service to answer (the SynchronousAdapter waits 2 seconds, so that is sufficient). Since
+            // it is the get()-call itself that throws, a TimeoutException will be raised.
+            thrown.expect(TimeoutException.class);
+
+            // This shall not go through, but throw
+            Reply<DataTO> result = future.get(75, TimeUnit.MILLISECONDS);
+
+            Assert.fail("The future.get(75 ms) call should not have gone through, since the timeout specified is"
+                    + " too short for the service to answer. The result was: " + result);
+        }
+    }
+
+}
