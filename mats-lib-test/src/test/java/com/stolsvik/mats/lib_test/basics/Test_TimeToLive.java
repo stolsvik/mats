a44885fdc13b0d7be5f3a9c34a01119ee00a5ddd
Endre Stølsvik <endre@stolsvik.com>
2020-01-05 20:46:07 +0100

    Seeing if upping some timeouts can make tests pass.

--- mats-lib-test/src/test/java/com/stolsvik/mats/lib_test/basics/Test_TimeToLive.java
+++ mats-lib-test/src/test/java/com/stolsvik/mats/lib_test/basics/Test_TimeToLive.java
@@ -29,7 +29,7 @@ public class Test_TimeToLive extends MatsBasicTest {
                 (context, dto) -> {
                     if ("DELAY".equals(dto.string)) {
                         try {
-                            Thread.sleep(100);
+                            Thread.sleep(300);
                         }
                         catch (InterruptedException e) {
                             throw new IllegalStateException(e);
@@ -61,7 +61,7 @@ public class Test_TimeToLive extends MatsBasicTest {
 
     @Test
     public void testWithTimeToLive() {
-        doTest(75, 2);
+        doTest(100, 2);
     }
 
     private void doTest(long timeToLive, int expectedMessages) {
