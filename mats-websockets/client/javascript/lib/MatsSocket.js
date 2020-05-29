3302a678bae02ab97a0a57cd8f0e641eed945cfe
Ståle Undheim <staale@staale.org>
2020-05-29 14:35:24 +0200

    Set version to 0.12.0
    I contribute this material in accordance with the signed SCA.

--- mats-websockets/client/javascript/lib/MatsSocket.js
+++ mats-websockets/client/javascript/lib/MatsSocket.js
@@ -1222,7 +1222,7 @@
      * @constructor
      */
     function MatsSocket(appName, appVersion, urls, config) {
-        let clientLibNameAndVersion = "MatsSocket.js,v0.11.0";
+        let clientLibNameAndVersion = "MatsSocket.js,v0.12.0";
 
         // :: Validate primary arguments
         if (typeof appName !== "string") {
