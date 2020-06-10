53439a9d2a1faf2e4af39fd181e26914faffde87
Ståle Undheim <staale@staale.org>
2020-06-10 13:28:18 +0200

    Export all needed classes from the package.
    I contribute this material in accordance with the signed SCA.

--- mats-websockets/client/dart/lib/mats_socket.dart
+++ mats-websockets/client/dart/lib/mats_socket.dart
@@ -2,10 +2,15 @@
 library mats_socket;
 
 export 'src/MatsSocket.dart';
-export 'src/MessageType.dart';
-export 'src/ConnectionEvent.dart';
-export 'src/MessageEvent.dart';
-export 'src/MatsSocketEnvelopeDto.dart';
-export 'src/ReceivedEvent.dart';
-export 'src/MatsSocketCloseEvent.dart';
+export 'src/MatsSocketPlatform.dart';
 export 'src/AuthorizationRequiredEvent.dart';
+export 'src/ConnectionEvent.dart';
+export 'src/ErrorEvent.dart';
+export 'src/InitiationProcessedEvent.dart';
+export 'src/MatsSocketCloseEvent.dart';
+export 'src/MatsSocketEnvelopeDto.dart';
+export 'src/MessageEvent.dart';
+export 'src/MessageType.dart';
+export 'src/PingPong.dart';
+export 'src/ReceivedEvent.dart';
+export 'src/SubscriptionEvent.dart';
