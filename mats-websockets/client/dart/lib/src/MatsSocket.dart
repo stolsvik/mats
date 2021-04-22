6c7f6481ec051df07376ad2d897a05595030f51d
Endre Stølsvik <endre@stolsvik.com>
2021-04-23 00:15:23 +0200

    Version bump (RC): 0.16.1.2021-02-26 -> RC-0.16.2.2021-04-22

--- mats-websockets/client/dart/lib/src/MatsSocket.dart
+++ mats-websockets/client/dart/lib/src/MatsSocket.dart
@@ -22,7 +22,7 @@ final Logger _logger = Logger('MatsSocket');
 const String ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // JSON-non-quoted and visible Alphabet: 92 chars.
 const String JSON_ALPHABET = '!#\$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-const String CLIENT_LIB_NAME_AND_VERSION = 'MatsSocket.dart,v0.16.1 (2021-02-26)';
+const String CLIENT_LIB_NAME_AND_VERSION = 'MatsSocket.dart,vRC-0.16.2 (2021-04-22)';
 
 typedef SessionClosedEventListener = Function(MatsSocketCloseEvent);
 typedef PingPongListener = Function(PingPong);
--- mats-websockets/client/javascript/lib/MatsSocket.js
+++ mats-websockets/client/javascript/lib/MatsSocket.js
@@ -1224,7 +1224,7 @@
      * @constructor
      */
     function MatsSocket(appName, appVersion, urls, config) {
-        let clientLibNameAndVersion = "MatsSocket.js,v0.16.1 (2021-02-26)";
+        let clientLibNameAndVersion = "MatsSocket.js,vRC-0.16.2 (2021-04-22)";
 
         // :: Validate primary arguments
         if (typeof appName !== "string") {
