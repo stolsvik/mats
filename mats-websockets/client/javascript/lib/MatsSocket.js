0afa59fc559f2e5cb6c7a70f6a3173f62b8044c6
Endre Stølsvik <endre@stolsvik.com>
2021-05-20 13:10:33 +0200

    Version bump: 0.16.2.2021-05-18 -> 0.16.3.2021-05-20

--- mats-websockets/client/javascript/lib/MatsSocket.js
+++ mats-websockets/client/javascript/lib/MatsSocket.js
@@ -1224,7 +1224,7 @@
      * @constructor
      */
     function MatsSocket(appName, appVersion, urls, config) {
-        let clientLibNameAndVersion = "MatsSocket.js,v0.16.2 (2021-05-18)";
+        let clientLibNameAndVersion = "MatsSocket.js,v0.16.3 (2021-05-20)";
 
         // :: Validate primary arguments
         if (typeof appName !== "string") {
